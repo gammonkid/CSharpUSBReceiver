26cc853daadc33d70977f6931de2351adc4f95f9
Collin Plumlee <gammonkid@gmail.com>
2018-10-28 00:57:16 -0500

    new base for everything

---
+++ WFF Generic HID Demo 3/WFF Generic HID Demo 3/DeviceConnections.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFF_Generic_HID_Demo_3
+{
+    public partial class DeviceConnections : Form
+    {
+        public DeviceConnections()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WFF Generic HID Demo 3/WFF Generic HID Demo 3/Players.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WFF_Generic_HID_Demo_3
+{
+    partial class Players
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Players
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 412);
+            this.Name = "Players";
+            this.Text = "Players";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
